49144c65a315aee460ae5b1bdaf0992eb8075cbf
Sunny <cuthanhson27@gmail.com>
2023-09-14 16:24:35 +0700

    Implement view user model

--- addons/manage_user_info/models/user_info.py
+++ addons/manage_user_info/models/user_info.py
@@ -1,16 +1,24 @@
-from odoo import models, fields
+from odoo import models, fields,api
 
 class UserInfo(models.Model):
   _name = 'user.info'
   _description = 'User Info'
   
-  first_name = fields.Char('Firts name', required=True)
-  surname = fields.Char('Surname', required=True)
-  # field email available before
-  user_id = fields.Many2one('res.users', required=True, ondelete='cascade', delegate=True)
-  user_info_major_id = fields.Many2one('user.info.major',string='Major', required=True, ondelete='cascade')
-  user_info_department_id = fields.Many2one('user.info.department',string='Department', required=True, ondelete='cascade')
-  user_info_contact_id = fields.Many2one('user.info.contact',string='Personal Contact', required=True, ondelete='cascade')
-  user_info_class_id = fields.Many2one('user.info.class',string='Class', required=True, ondelete='cascade')
+  first_name = fields.Char('Firts Name',compute='_compute_name_parts', store=True)
+  sur_name = fields.Char('Sur Name',compute='_compute_name_parts', store=True)
+  user_id = fields.Many2one('res.users',string="User", required=True, ondelete='cascade', delegate=True)
+  # user_info_major_id = fields.Many2one('user.info.major',string='Major', required=True, ondelete='cascade')
+  # user_info_department_id = fields.Many2one('user.info.department',string='Department', required=True, ondelete='cascade')
+  # user_info_contact_id = fields.Many2one('user.info.contact',string='Personal Contact', required=True, ondelete='cascade')
+  # user_info_class_id = fields.Many2one('user.info.class',string='Class', required=True, ondelete='cascade')
+  
+  @api.depends('name')
+  def _compute_name_parts(self):
+    for user in self:
+      if user.name:
+        name_parts = user.name.split(" ")
+        user.first_name = name_parts[0]
+        user.sur_name = " ".join(name_parts[1:]) 
+
   
   
