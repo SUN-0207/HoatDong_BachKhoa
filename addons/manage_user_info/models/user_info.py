5b3667db23543f9612f249c45c33a49264cb33c0
nhaTahn <lnth1720@gmail.com>
2023-09-16 12:35:49 +0700

    district bug

--- addons/manage_user_info/models/user_info.py
+++ addons/manage_user_info/models/user_info.py
@@ -2,34 +2,16 @@ from odoo import models, fields,api
 
 class UserInfo(models.Model):
   _name = 'user.info'
-
   _description = 'User Info'
-
   first_name = fields.Char('Firts Name',compute='_compute_name_parts', store=True)
   sur_name = fields.Char('Sur Name',compute='_compute_name_parts', store=True)
+  
   user_id = fields.Many2one('res.users',string="User", required=True, ondelete='cascade', delegate=True)
   # user_info_major_id = fields.Many2one('user.info.major',string='Major', required=True, ondelete='cascade')
   # user_info_department_id = fields.Many2one('user.info.department',string='Department', required=True, ondelete='cascade')
-  # user_info_contact_id = fields.One2Many('user.info.contact','contact_id', required=True, ondelete='cascade')
+  # user_info_contact_id = fields.One2many('user.info.contact','contact_id')
   # user_info_class_id = fields.Many2one('user.info.class',string='Class', required=True, ondelete='cascade')
-  
-  student_id = fields.Char(string="Student ID", required=True)
-  phone_number = fields.Char(string="Phone number", required=True)
-  gender = fields.Char(string="Gender", required=True)
-  birth_date = fields.Char(string="Birth Day", required=True)
-  nation = fields.Char(string="Nation")
-  personal_email = fields.Char(string="Personal Email", required=True)
-  religion = fields.Char(string="Religion")
-  national_id = fields.Char(string="National Id", required=True)
-  native_address = fields.Char(string="Native Address")
-  native_address_specific = fields.Char(string="Native Address Specific")
-  permanent_address = fields.Char(string="Permanent Address")
-  permanent_address_specific = fields.Char(string="Permanent Address Specific")
-  place_union = fields.Char(string="Place Union")
-  date_at_union = fields.Char(string="Date At Union")
-  place_communist_party = fields.Char(string="Place Communist Party")
-  date_at_student_association = fields.Char(string="Date at Student Association")
-
+ 
   @api.depends('name')
   def _compute_name_parts(self):
     for user in self:
@@ -37,6 +19,6 @@ class UserInfo(models.Model):
         name_parts = user.name.split(" ")
         user.first_name = name_parts[0]
         user.sur_name = " ".join(name_parts[1:]) 
-
+  
   
   
--- addons/manage_user_info/models/user_info_contact.py
+++ addons/manage_user_info/models/user_info_contact.py
@@ -1,25 +1,98 @@
-from odoo import models, fields
+from odoo import models, fields, api
+import json
+import requests
+
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class UserInfoContact(models.Model):
   _name = 'user.info.contact'
   _description = 'User Info Contact'
-  
-#   student_id = fields.Char(string="Student ID", required=True)
-#   phone_number = fields.Char(string="Phone number", required=True)
-#   gender = fields.Char(string="Gender", required=True)
-#   birth_date = fields.Char(string="Birth Day", required=True)
-#   nation = fields.Char(string="Nation")
-#   personal_email = fields.Char(string="Personal Email", required=True)
-#   religion = fields.Char(string="Religion")
-#   national_id = fields.Char(string="National Id", required=True)
-#   native_address = fields.Char(string="Native Address")
-#   native_address_specific = fields.Char(string="Native Address Specific")
-#   permanent_address = fields.Char(string="Permanent Address")
-#   permanent_address_specific = fields.Char(string="Permanent Address Specific")
-#   place_union = fields.Char(string="Place Union")
-#   date_at_union = fields.Char(string="Date At Union")
-#   place_communist_party = fields.Char(string="Place Communist Party")
-#   date_at_student_association = fields.Char(string="Date at Student Association")
+  # _inherits = ['user.info']
 
-#   contact_id = fields.Many2one('user.info', string="User id")
+  contact_id = fields.Many2one('user.info', string="User id", ondelete='cascade', delegate=True)
+
+  student_id = fields.Char(string="Student ID", required=True)
+  phone_number = fields.Char(string="Phone number", required=True)
+  gender = fields.Selection([('male', 'Male'),('female', 'Female')], required=True)
+  birth_date = fields.Char(string="Birth Day", required=True)
+  nation = fields.Char(string="Nation")
+  personal_email = fields.Char(string="Personal Email", required=True)
+  religion = fields.Char(string="Religion")
+  national_id = fields.Char(string="National Id", required=True)
+  native_address = fields.Char(string="Native Address")
+  native_address_specific = fields.Char(string="Native Address Specific")
+  permanent_address = fields.Char(string="Permanent Address")
+  permanent_address_specific = fields.Char(string="Permanent Address Specific")
+  place_union = fields.Char(string="Place Union")
+  date_at_union = fields.Char(string="Date At Union")
+  place_communist_party = fields.Char(string="Place Communist Party")
+  date_at_student_association = fields.Char(string="Date at Student Association")
   
+  province = fields.Selection(
+        selection='_get_province_data',
+        string='Province',
+  )
+
+  district = fields.Selection(
+      selection='_get_districts',
+      string='District'
+  )
+
+  # @api.model
+  # def _get_province_data(self):
+  #   options = []
+  #   response = requests.get("https://provinces.open-api.vn/api/p/")
+  #   response.raise_for_status()
+  #   data = response.json()
+
+  #   for province_data in data:
+  #     options.append((province_data['code'], province_data['name']))
+
+  #   return options
+  
+  # @api.depends('province')
+  # def _get_districts(self):    
+  #   if self.province:
+  #     options = []
+  #     response = requests.get("https://provinces.open-api.vn/api/d/")
+  #     response.raise_for_status()
+  #     data = response.json()
+  #     for districts in data:
+  #       if(districts['province_code'] == self.province):
+  #         options.append((districts['code'], districts['name']))
+      
+  #   return options
+  
+  @api.model
+  def _get_province_data(self):
+        options = []
+        response = requests.get("https://provinces.open-api.vn/api/p/")
+        response.raise_for_status()
+        data = response.json()
+
+        for province_data in data:
+            options.append((province_data['code'], province_data['name']))
+        return options
+
+  @api.model
+  def _get_districts(self):
+    print("outside")
+    if self.province:
+      print("inside")
+      options = []
+      response = requests.get(f"https://provinces.open-api.vn/api/d/{self.province}")
+      response.raise_for_status()
+      data = response.json()
+      lst = data['districts']
+      for district_data in lst:
+        options.append((district_data['code'], district_data['name']))
+      return options
+    else:
+        return []
+
+  @api.onchange('province')
+  def _onchange_province(self):
+    if self.province:
+      self.district = False
