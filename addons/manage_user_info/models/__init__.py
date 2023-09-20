64164db711ef1a1dd6a0637f64a525853455b38e
Sunny <cuthanhson27@gmail.com>
2023-09-20 10:40:20 +0700

    refactor code

--- addons/manage_user_info/models/__init__.py
+++ addons/manage_user_info/models/__init__.py
@@ -1,6 +1,5 @@
 from . import user_info
 from . import user_info_class
-from . import user_info_contact
 from . import user_info_department
 from . import user_info_major
 from . import user_info_address
