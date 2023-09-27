429230e3d342abc99b747ca0be7ae28237e1c8d0
nhaTahn <lnth1720@gmail.com>
2023-09-27 20:13:40 +0700

    update view form

--- addons/manage_user_info/models/user_info.py
+++ addons/manage_user_info/models/user_info.py
@@ -8,22 +8,19 @@ class UserInfo(models.Model):
   
   user_id = fields.Many2one('res.users', string='User', readonly=True)
   
-  name= fields.Char(related='user_id.name', string="Name")
+  name = fields.Char(related='user_id.name', string="Name", store=True)
+  first_name = fields.Char('First Name', compute='_compute_name_parts', inverse='_inverse_name')
+  sur_name = fields.Char('Sur Name', compute='_compute_name_parts', inverse='_inverse_name')
   email= fields.Char(related='user_id.email', string="Email")
   avatar = fields.Binary(string='Avatar')
-  first_name = fields.Char('First Name',compute='_compute_name_parts')
-  sur_name = fields.Char('Sur Name',compute='_compute_name_parts')
 
-  
   phone_number = fields.Char(string="Phone number")
   gender = fields.Selection([('male', 'Male'),('female', 'Female')])
   birth_date = fields.Date(string="Birth Day")
   nation = fields.Char(string="Nation")
   personal_email = fields.Char(string="Personal Email")
-  religion = fields.Selection(common_constants.RELIGION, string="Religion")
-
-  ethnicity = fields.Selection(common_constants.ETHNICITY, string='Ethnicity')
-  national_id = fields.Char(string="National Id")
+  religion = fields.Selection(common_constants.RELIGION, string="Religion", translate=False)
+  ethnicity = fields.Selection(common_constants.ETHNICITY, string='Ethnicity', translate=False)
 
   date_communist_party= fields.Date(string="Date At Communist Party")
   place_communist_party = fields.Char(string="Place Communist Party")
@@ -31,23 +28,28 @@ class UserInfo(models.Model):
   place_union = fields.Char(string="Place Union")
   date_at_student_association = fields.Date(string="Date at Student Association")
   
+  national_id = fields.Char(string="National Id")
+  national = fields.Selection(common_constants.NATIONAL,string="National", translate=False)
+  
   native_address = fields.Char(string="Native Address")
   native_address_specific = fields.Char(string="Native Address Specific")
-  province_id_native = fields.Many2one('user.province.info', 'Province (Native)', widget='selection')
-  district_id_native = fields.Many2one('user.district.info', 'District (Native)', domain="[('province_id', '=', province_id_native)]", widget='selection')
-  ward_id_native = fields.Many2one('user.ward.info', 'Ward (Native)', domain="[('district_id', '=', district_id_native)]", widget='selection')
+  province_id_native = fields.Many2one('user.province.info', string='Province')
+  district_id_native = fields.Many2one('user.district.info', string='District', domain="[('province_id', '=', province_id_native)]")
+  ward_id_native = fields.Many2one('user.ward.info', string='Ward', domain="[('district_id', '=', district_id_native)]")
   
   permanent_address = fields.Char(string="Permanent Address")
   permanent_address_specific = fields.Char(string="Permanent Address Specific")
-  province_id_permanent = fields.Many2one('user.province.info', 'Province (Permanent)', widget='selection')
-  district_id_permanent = fields.Many2one('user.district.info', 'District (Permanent)', domain="[('province_id', '=', province_id_permanent)]", widget='selection')
-  ward_id_permanent = fields.Many2one('user.ward.info', 'Ward (Permanent)', domain="[('district_id', '=', district_id_permanent)]", widget='selection')
+  province_id_permanent = fields.Many2one('user.province.info', string='Province')
+  district_id_permanent = fields.Many2one('user.district.info', string='District', domain="[('province_id', '=', province_id_permanent)]")
+  ward_id_permanent = fields.Many2one('user.ward.info', string='Ward', domain="[('district_id', '=', district_id_permanent)]")
 
   student_id = fields.Char(string="Student ID")
+  falcutian_id = fields.Char(string="Falcutian ID")
+
   user_info_department_id = fields.Many2one('user.info.department',string='Department')
-  year_in = fields.Char(string="Year in", compute="_compute_year_in")
+  year_in = fields.Char(string="Year in", compute="_compute_year_in", store=True)
   user_info_major_id = fields.Many2one('user.info.major',string='Major')
-  user_info_class_id = fields.Many2one('user.info.class',string='Class')
+  user_info_class_id = fields.Many2one('user.info.class',string='Class', translate=False)
 
   personal_email_valid = fields.Boolean(string='Invalid Email',default=True)
   
@@ -73,14 +75,61 @@ class UserInfo(models.Model):
    
   @api.depends('name')
   def _compute_name_parts(self):
-    for user in self:
-      if user.name:
-        name_parts = user.name.split(" ")
-        user.first_name = name_parts[0]
-        user.sur_name = " ".join(name_parts[1:]) 
+    for record in self:
+        if record.name:
+            name_parts = record.name.split(" ")
+            record.first_name = name_parts[0] if name_parts else ''
+            record.sur_name = " ".join(name_parts[1:]) if len(name_parts) > 1 else ''
+        else:
+            record.first_name = ''
+            record.sur_name = ''
+
+  def _inverse_name(self):
+    for record in self:
+        record.name = record.first_name + ' ' + record.sur_name if record.first_name or record.sur_name else ''
+
+  @api.onchange('national')
+  def on_national_change(self):
+    if self.national != '2':
+      self.province_id_native = False
+      self.district_id_native = False
+      self.ward_id_native = False
+
+  @api.onchange('province_id_native')
+  def on_province_native_change(self):
+    if self.province_id_native:
+      self.district_id_native = False
+      self.ward_id_native = False
   
+  @api.onchange('self.district_id_native')
+  def on_district_native_change(self):
+    if self.district_id_native:
+      self.ward_id_native = False
+  
+  @api.onchange('province_id_permanent')
+  def on_province_permanent_change(self):
+    if self.province_id_permanent:
+      self.district_id_permanent = False
+      self.ward_id_permanent= False
+  
+  @api.onchange('district_id_permanent')
+  def on_district_permanent_change(self):
+    if self.district_id_permanent:
+      self.ward_id_native = False
+
+  @api.onchange('user_info_department_id')
+  def on_department_change(self):
+    if self.user_info_department_id:
+      self.user_info_major_id = False
+      self.user_info_class_id = False
+  
+  @api.onchange('user_info_major_id')
+  def on_major_change(self):
+    if self.user_info_major_id:
+      self.user_info_class_id = False
+
   @api.onchange('personal_email')
-  def _validate_email(self):
+  def on_personal_email_change(self):
     pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
     if self.personal_email and not re.match(pattern, self.personal_email):
       return {
@@ -91,7 +140,7 @@ class UserInfo(models.Model):
       }
 
   @api.onchange('student_id')
-  def _onchange_number_field(self):
+  def on_student_id_change(self):
     pattern = r'^0?\d{7}$'
     if self.student_id and not self.student_id.isdigit():
       return {
@@ -108,15 +157,6 @@ class UserInfo(models.Model):
         }
       }
   
-  @api.depends('student_id')
-  def _compute_year_in(self):
-    for record in self:
-        if record.student_id:
-            year_prefix = record.student_id[:2]
-            record.year_in = str(int(year_prefix) + 2000)
-        else:
-          record.year_in=""
-
   @api.onchange('phone_number')
   def _onchange_phone_field(self):
     pattern = r'^0?\d{10}$'
@@ -127,6 +167,16 @@ class UserInfo(models.Model):
         'message': 'Invalid phone number'
         }
       }
+
+  @api.depends('student_id')
+  def _compute_year_in(self):
+    for record in self:
+        if record.student_id:
+            year_prefix = record.student_id[:2]
+            record.year_in = str(int(year_prefix) + 2000)
+        else:
+          record.year_in=""
+
   
     
 class ResUsers(models.Model):
