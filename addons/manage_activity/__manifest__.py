ad96e96e05bd987354dc6ce0a2fbb24c7a9487cd
Sunny <cuthanhson27@gmail.com>
2023-10-08 13:32:34 +0700

    init module file

---
+++ addons/manage_activity/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Manage Activity",
+    "description": """Manage Activity""",
+    "summary": "Manage Activity",
+    "version": "16.0.2",
+    'author': 'TST',
+    'website': "https://www.odoo.com",
+    "depends": ['base','base_setup'],
+    "data": [],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
