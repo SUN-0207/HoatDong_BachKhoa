9d0915dcc0eefff05b995be4386f0b0bbd300ac0
tieu <thaingoctieu2002@gmail.com>
2023-10-16 19:10:33 +0700

    okai  ADMIN login page ()

--- addons/code_backend_theme/controllers/custom_login_admin.py
+++ addons/code_backend_theme/controllers/custom_login_admin.py
@@ -4,5 +4,5 @@ from odoo.http import request
 class CustomLoginAdmin(http.Controller):
 
     @http.route('/web/login/admin', type='http', auth="public", website=True)
-    def custom_login_page(self):
-        return request.render('code_backend_theme.custom_login_admin')
+    def custom_login_page(self, **kw):
+        return request.render('code_backend_theme.custom_login_admin', {})
