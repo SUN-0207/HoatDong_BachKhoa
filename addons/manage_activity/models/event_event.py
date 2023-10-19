2a870054f39fa4668a8306aefe4364b8b4b37c47
nhaTahn <nhaTahn>
2023-10-19 08:17:57 +0700

    add checkbox

--- addons/manage_activity/models/event_event.py
+++ addons/manage_activity/models/event_event.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api,SUPERUSER_ID
+from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 class EventEvent(models.Model):
   _inherit = 'event.event'
@@ -6,30 +6,7 @@ class EventEvent(models.Model):
   user_id = fields.Many2one('res.users', string='User', readonly=True)
   activity_manager =  fields.Many2many('user.department.admin', string='Giam sat')
   created_by_name = fields.Char(string="Hoat dong duoc tao boi ", store=True, default = lambda self: self.env.user.name)
-
-  #Xoa default khong duoc => chac xoa tay
-  @api.model
-  def uninstall_hook(self):
-        unwanted_stages = ['event_stage_new', 'event_stage_booked', 'event_stage_announced', 'event_stage_done', 'event_stage_cancelled']
-        stages = self.env['event.stage'].search([('id', 'in', unwanted_stages)])
-        stages.unlink()
   
-  # status_activity = fields.Selection(
-  #   string="Trạng thái",
-  #   selection=[
-  #       ('new', 'Mới'),
-  #       ('need_update', 'Bổ sung'),
-  #       ('accept', 'Đã duyệt'),
-  #       ('cancel', 'Đã huỷ'),
-  #       ('registration', 'Mở đăng ký'),
-  #       ('inprogress', 'Đang diễn ra'),
-  #       ('completed', 'Đã kết thúc')
-  #   ],
-  #   readonly=True,
-  #   copy=False,
-  #   default='new',
-  # )
-
   #Kanban (cai tron tron goc phai tren)
   # kanban_state = fields.Selection([('normal', 'In Progress'), ('done', 'Done'), ('blocked', 'Blocked')], default='normal', copy=False)
   # kanban_state_label = fields.Char(
@@ -38,13 +15,18 @@ class EventEvent(models.Model):
 
   date_begin_registration = fields.Datetime(string='Ngày bắt đầu đăng ký', required=True, tracking=True)
   date_end_registration = fields.Datetime(string='Ngày kết thúc đăng ký', required=True, tracking=True)
+  #Chuan bi lam template (de cho 1 so cai nhu drl vs ctxh)
   max_social_point = fields.Char(string="Số ngày CTXH tối đa")
   max_tranning_point = fields.Integer(string="DRL toi da")
 
   description = fields.Text(string="Nội dung hoạt động", widget="html" )
   attach_file = fields.Many2many('ir.attachment', string='Attachments', widget='many2many_binary')
 
-  department_can_register = fields.Many2many('user.info.department', string='Department', default='False')
+  is_for_all_students = fields.Boolean(string="Danh cho toan bo sinh vien", default=True)
+  is_maximize_department = fields.Boolean(string="Gioi han don vi tham gia", default=False)
+  is_maximize_major = fields.Boolean(string="Gioi han chuyen nganh tham gia", default=False)
+  is_maximize_year = fields.Boolean(string="Gioi han khoa tham gia", default=False)
+  department_can_register = fields.Many2many('user.info.department', string='Department', default=False)
   major_can_register = fields.Many2many('user.info.major', string='Major')
   year_can_register = fields.Many2many('user.info.year', string='Years')
 
@@ -58,6 +40,17 @@ class EventEvent(models.Model):
             if activity.date_begin_registration >= activity.date_end:
                 raise ValidationError('Ngay bat dau dang ky phai truoc ngay ket thuc')
 
+  @api.onchange('is_for_all_students','is_maximize_department')
+  def _change_checkbox(self):
+    for record in self:
+      if record.is_for_all_students == True:
+        record.is_maximize_department = False
+        record.is_maximize_major = False
+        record.is_maximize_year = False
+      if record.is_maximize_department == False:
+        if record.is_maximize_major == True:
+          record.is_maximize_major = False
+      
 
 class ResUsers(models.Model):
   _inherit = ['res.users']
