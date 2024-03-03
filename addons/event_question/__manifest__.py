c026fd54f4a704ffcac89871c228d5e998eb3f0f
tieu <thaingoctieu2002@gmail.com>
2024-03-03 19:32:38 +0700

    event question: show question for user

---
+++ addons/event_question/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    "name": "Event Questions",
+    "description": """Event custom questions""",
+    "summary": "Manage Activity's addon module",
+    "version": "16.0.2",
+    'author': 'TST',
+    'website': "https://www.odoo.com",
+    "depends": [
+        'base',
+        'base_setup',
+        'web', 
+        'event',
+        'manage_activity',
+    ],
+    "data": [
+        'security/ir.model.access.csv',
+        'views/event_views.xml',
+        'views/event_question_views.xml',
+        'views/event_registration_views.xml',
+        'wizard/event_registration_wizard_views.xml',
+        'wizard/event_registration_wizard_answers.xml'
+    ],
+    'assets': {
+       'web.assets_backend': [
+        ],
+    },
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
